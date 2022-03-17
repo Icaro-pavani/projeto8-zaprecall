9bd9ddd92628bd996c25a709618c293789ac6a85
Icaro-pavani <icaro.pavani79@gmail.com>
2022-03-17 15:51:20 -0300

    mudanca tela inicial implementada

--- src/components/TelaInicio.js
+++ src/components/TelaInicio.js
@@ -1,9 +1,9 @@
-export default function TelaInicio() {
+export default function TelaInicio(props) {
     return (
         <div className="tela-inicio">
             <img className="logo-inicio" src="imagens/logo.png" alt="logo" />
             <h1>ZapRecall</h1>
-            <button>Iniciar Recall!</button>
+            <button onClick={props.fncAbrirDeck}>Iniciar Recall!</button>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import ReactDom from 'react-dom';
+import React from 'react';
 
 import TelaInicio from './components/TelaInicio';
 import TelaPerguntas from './components/TelaPerguntas';
 
+
 function App() {
-    return (
-        <>
-          <TelaInicio />
-          <TelaPerguntas />
-        </>
-    )
+  const [abrirDeck, setAbrirDeck] = React.useState(false);
+
+  function acionaAbrirDeck() {
+    setAbrirDeck(true);
+  }
+
+  return (
+      <>
+        {!abrirDeck ? <TelaInicio fncAbrirDeck={acionaAbrirDeck}/> : <TelaPerguntas />}
+      </>
+  )
 }
 
 ReactDom.render(<App />, document.querySelector(".root"));
