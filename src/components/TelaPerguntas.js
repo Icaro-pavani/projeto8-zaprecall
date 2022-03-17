6aa54c338dbea022c56468cf350c718af3f4f852
Icaro-pavani <icaro.pavani79@gmail.com>
2022-03-17 02:07:20 -0300

    topo da tela de perguntas implementado

---
+++ src/components/TelaPerguntas.js
@@ -0,0 +1,10 @@
+export default function TelaPerguntas() {
+    return (
+        <div className="tela-perguntas">
+            <div className="topo-perguntas escondido">
+                <img className="logo-perguntas" src="imagens/logo-pequeno.png" alt="logo-pequeno" />
+                <h2>ZapRecall</h2>
+            </div>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import ReactDom from 'react-dom';
 
-import TelaInicio from './components/TelaInicio.js';
+import TelaInicio from './components/TelaInicio';
+import TelaPerguntas from './components/TelaPerguntas';
 
 function App() {
     return (
-        <TelaInicio />
+        <>
+          <TelaInicio />
+          <TelaPerguntas />
+        </>
     )
 }
 
